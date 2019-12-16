f87ae199e5f89619bd1d49eaeb10ea9f73ea519f
Henna Singh <hennasingh04@gmail.com>
2019-12-16 12:30:00 +0000

    added realm model classes to persist data.

--- app/src/main/java/com/pluralsight/realmdemo/model/User.java
+++ app/src/main/java/com/pluralsight/realmdemo/model/User.java
@@ -1,6 +1,63 @@
 package com.pluralsight.realmdemo.model;
 
 
-public class User {
+import io.realm.RealmObject;
+import io.realm.annotations.Ignore;
+import io.realm.annotations.PrimaryKey;
 
+public class User extends RealmObject {
+
+    //define field variables
+
+    @PrimaryKey //also provides @Index annotation
+    private String id; //no auto-increment field
+
+    private String name;
+
+    private int age;
+
+    private SocialAccount socialAccount;
+
+    @Ignore // not safed in database
+    private String tempData;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public SocialAccount getSocialAccount() {
+        return socialAccount;
+    }
+
+    public void setSocialAccount(SocialAccount socialAccount) {
+        this.socialAccount = socialAccount;
+    }
+
+    public String getTempData() {
+        return tempData;
+    }
+
+    public void setTempData(String tempData) {
+        this.tempData = tempData;
+    }
 }
